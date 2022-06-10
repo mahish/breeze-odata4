a9e817cbc1b43a396e652b4032874b63a6629c99
Jiří Maha <nezapomenout@gmail.com>
2022-06-10 12:02:40 +0200

    Fix DataType.Date to format Odata properly

--- src/datatypes/setups/date-datatype-setup.ts
+++ src/datatypes/setups/date-datatype-setup.ts
@@ -1,30 +1,12 @@
-import { DataType } from 'breeze-client';
-import { EdmDate } from '../models';
-
-import { BaseDataTypeSetup } from './base-datatype-setup';
-
-/**
- * Supports ISO 8601 formatted dates, allowing for year 0000 and negative years.
- */
-const DateRegex = /(-\d)?\d{4}-\d{2}-\d{2}/;
+import { EdmDate } from "../models";
+import { BaseDataTypeSetup } from "./base-datatype-setup";
 
 /**
  * @classdesc Sets up the breeze DataType.Date enum symbol.
+ * @see {DataType.Date}
  */
 export class DateDataTypeSetup extends BaseDataTypeSetup {
-    public name = 'Date';
+  public name = "Date";
 
-    public addSymbol = () => {
-        const result = new DataType({
-            defaultValue: '0000-01-01',
-            fmtOData: (val: any) => val ? EdmDate.create(val).toString() : null,
-            isDate: true,
-            name: this.name,
-            parseRawValue: EdmDate.create
-        });
-
-        DataType[this.name] = result;
-
-        return result;
-    }
+  public fmtOData = (val: any) => (val ? EdmDate.create(val).toString() : null);
 }
